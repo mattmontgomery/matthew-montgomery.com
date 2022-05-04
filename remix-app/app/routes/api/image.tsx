c1a666e8f1b7e1698d58759e215a91c9061190b3
Matt Montgomery <mmontgomery@deseretdigital.com>
2022-05-03 20:08:38 -0600

    tries memory cache

--- remix-app/app/routes/api/image.tsx
+++ remix-app/app/routes/api/image.tsx
@@ -1,12 +1,12 @@
 import type { LoaderFunction } from "@remix-run/server-runtime";
-import { imageLoader, DiskCache } from "remix-image/server";
+import { imageLoader, MemoryCache } from "remix-image/server";
 import { sharpTransformer } from "~/utils/sharp-transformer";
 
 export const loader: LoaderFunction = ({ request }) => {
   return imageLoader(
     {
       selfUrl: "http://localhost:3000",
-      cache: new DiskCache(),
+      cache: new MemoryCache(),
       transformer: sharpTransformer,
     },
     request
