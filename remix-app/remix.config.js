4b25f7ea0f60e57b5048b1e5d6267ded02df6218
Matt Montgomery <mmontgomery@deseretdigital.com>
2022-05-03 19:43:20 -0600

    chore: adds vercel as a build target

--- remix-app/remix.config.js
+++ remix-app/remix.config.js
@@ -14,6 +14,7 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "vercel",
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
 };
